a9d9995fe0b2391470029f191df77cfcd4cf7e12
eddhurst <eddhurst@users.noreply.github.com>
2025-10-24 21:37:02 +0100

    gitkeep files

--- src/locations/hakone.ts
+++ src/locations/hakone.ts
@@ -7,18 +7,21 @@ const Hakone = {
   description: '3 days',
   tiles: [
     { type: 'image', image: `${ASSET_CDN}/assets/hakone/hakoneFerry.jpg`, wide: true },
-    { type: 'image', image: `${ASSET_CDN}/assets/hakone/hakoneTorii.jpg`, tall: true },
-    { 
-      type: 'grid',
-      images: [
-        { type: 'image', image: `${ASSET_CDN}/assets/hakone/20250911_095631.jpg`, wide: true },
-        { type: 'image', image: `${ASSET_CDN}/assets/hakone/20250913_074418.jpg`, tall: true },
-        { type: 'image', image: `${ASSET_CDN}/assets/hakone/20250911_165935.jpg`, wide: true },
-      ]
+    {
+      type: 'quote',
+      text: 'Hakone is a mountainous town in Japan\'s Kanagawa Prefecture, known for its hot springs, natural beauty, and views of Mount Fuji. It\'s a popular destination for both tourists and locals seeking relaxation and outdoor activities.'
     },
+    
+    { type: 'image', image: `${ASSET_CDN}/assets/hakone/hakoneTorii.jpg`, tall: true },
+    { type: 'image', image: `${ASSET_CDN}/assets/hakone/20250913_074418.jpg`, tall: true },
+    { type: 'image', image: `${ASSET_CDN}/assets/hakone/IMG20250910173437.jpg`, tall: true },
+
+    { type: 'image', image: `${ASSET_CDN}/assets/hakone/20250911_095631.jpg`, wide: true },
+    
+    { type: 'image', image: `${ASSET_CDN}/assets/hakone/20250911_165935.jpg`, wide: true },
     { type: 'image', image: `${ASSET_CDN}/assets/hakone/20250912_160512.jpg`, wide: true },
     { type: 'image', image: `${ASSET_CDN}/assets/hakone/IMG20250910142915.jpg`, wide: true },
-    { type: 'image', image: `${ASSET_CDN}/assets/hakone/IMG20250910173437.jpg`, tall: true },
+    
     { type: 'image', image: `${ASSET_CDN}/assets/hakone/IMG20250910191553.jpg` },
     { type: 'image', image: `${ASSET_CDN}/assets/hakone/IMG20250910175000.jpg`, wide: true },
     { type: 'image', image: `${ASSET_CDN}/assets/hakone/IMG20250910192724.jpg` },
