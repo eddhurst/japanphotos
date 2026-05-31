41deb280a65cd64fec243fbb6787c3e5282a67ac
eddhurst <eddhurst@users.noreply.github.com>
2026-05-31 23:26:37 +0100

    feat: update config to be on root domain

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
-const ASSET_VERSION = '1.2.0';
+const ASSET_VERSION = '1.3.0';
 const IS_LOCAL = window.location.hostname === 'localhost';
-export const ASSET_CDN = IS_LOCAL ? '.' : `https://cdn.jsdelivr.net/gh/eddhurst/japanphotos@${ASSET_VERSION}`;
+export const ASSET_CDN = IS_LOCAL ? '.' : `https://cdn.jsdelivr.net/gh/eddhurst/zoeandedd@${ASSET_VERSION}`;
